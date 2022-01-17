b978b30a6a5d48e1b992d330dacc81f1cab805f3
Brandon <Brandonwirth@outlook.com>
2022-01-17 15:15:41 -0800

    GOT IT TO WORK!
    Added a reset function + a slider that lets you change the value
    
    to-do: have the canvas reset when user moves the slider

--- script.js
+++ script.js
@@ -1,24 +1,76 @@
-// const container = document.querySelector('canvas');
 
-// const block = document.createElement('div');
-// block.classList.add('block')
-// block.textContent = '!'
+const canvas = document.querySelector('#canvas');
+const btnReset = document.querySelector('#btn');
+let target;
 
-// container.appendChild(block);
-
-
-const container = document.querySelector('#canvas');
-
-const square = document.createElement('div');
+let square = document.createElement('div');
 square.classList.add('square');
 
-function makeSquare () {
-    for (let i = 0; i < 256; i++) {
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
+
+
+// On window launch it will create a canvas based on the default value or will rerun if the slider's value is changed
+function makeSquare (numSquare) {
+    for (let i = 0; i < numSquare; i++) {
     const square = document.createElement('div');
     square.classList.add('square');
-    container.appendChild(square);
+    canvas.appendChild(square);
     }
+}
+
+// check # of rows and columns and update styles.css 
+function makeGrid (numRow, numCol) {
+    canvas.style.setProperty('--numRows', numRow); 
+    canvas.style.setProperty('--numCols', numCol); 
+    for (let r = 0; r < numRow; r++) {
+        makeSquare(numCol);
+    }
+}
+
+function sliderValue(){
+    let number = slider.value
+    makeGrid(number,number);
+    console.log(number);
+}
+
+slider.onchange= function () {
+    number = slider.value;
+    sliderValue();
+}
+
+
+function changeColor (target) { //why can't this be square instead of target. Why did I have to create another variable here?
+    target.style.backgroundColor = '#2c2c54';
+}
+
+function resetColor (target) {
+    target.style.backgroundColor = 'white';
+}
+
+canvas.addEventListener("mouseover", function (e) {
+    target = e.target;
+
+    if (target.matches("div.square")) {
+        changeColor(target);
+    }
+    
+});
+
+function btnLog (btnReset) {
+}
+
+btnReset.onclick = function(){
+    console.log('YOU DID IT!');
+    window.location.reload();
 
 }
 
-makeSquare();
+sliderValue();
+
+
